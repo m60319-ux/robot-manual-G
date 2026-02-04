2796e6bf25e265af88d84665edc91bc163ef265b
m60319-ux <m60319@gmail.com>
2026-02-04 16:12:16 +0800

    Create data.en.js

---
+++ assets/manual/data/data.en.js
@@ -0,0 +1,41 @@
+window.FAQ_DATA_EN = {
+    "meta": {
+        "lang": "en",
+        "version": "1.0"
+    },
+    "categories": [
+        {
+            "id": "CAT-01",
+            "title": "Hardware Issues",
+            "subcategories": [
+                {
+                    "id": "SUB-01",
+                    "title": "Motor Anomaly",
+                    "questions": [
+                        {
+                            "id": "Q-001",
+                            "title": "Power Failure",
+                            "content": {
+                                "symptoms": [
+                                    "LED not lighting up123"
+                                ],
+                                "rootCauses": [
+                                    "Fuse blown",
+                                    "{{img:assets/images/img_1769763508765.png}}"
+                                ],
+                                "solutionSteps": [
+                                    "Replace fuse",
+                                    "{{img:assets/images/img_1769763514401.png}}"
+                                ],
+                                "notes": "Safety first\n{{img:assets/images/img_1769763517692.png}}",
+                                "keywords": [
+                                    "AL001"
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
